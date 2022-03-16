20ed76e7afd0d8f03d495ae4d7227561fda2a073
Rodrigo da Silva Lisboa <lisboasilva.educ@gmail.com>
2022-03-15 22:22:14 -0300

    EXERCICIO 15/03/2022

---
+++ modulo1/resolvendo-problemas/resolvendo-problemas.js
@@ -0,0 +1,32 @@
+`**Exercícios de fixação do conhecimento**
+    
+    Faça um resumo sobre quais são os 7 passos fundamentais para a resolução de problemas e como eles podem nos ajudar a solucionar nossos desafios de programação.
+
+**RESUMO DA AULA "RESOLVENDO PROBLEMAS**" 
+
+
+TODO PROBLEMA DEVE SER ABSTRAIDO A FIM DE SELECIONAR OS PASSOS REALMENTE ESSENCIAIS PARA A SUA RESOLUÇÃO SER CONSOLIDADE COM EFICIÊNCIA
+
+1 - INTERPRETAÇÃO DO ENUNCIADO
+Deve-se dar atenção ao que é realmente proposto pelo enunciado
+
+2 - SELEÇÃO DE INFORMAÇÕES IMPORTANTE 
+Deve-se selecionar as mais relevantes informações para que se garanta a realização rápida dos trabalhos 
+
+3 - PENSAR ENTRADAS E SAIDAS
+Deve-se ter em mente as informações a serem inseridas no código e o que será processado e retornado a partir do código para garantir a interatividade do mesmo 
+
+4 - FAZER UM ESQUEMA 
+Deve-se fazer um esquema organizacinal para abstrair melhor os passos de escrita de código e assim evitar erros que o comprometam. 
+
+5 - CRIAR UM ESBOÇO 
+O pontapé inicial é necessário, em primeiro caso o código pode ser feito de maneira singela e ao longo do desenvolvimento ser incrementado. Isso ajuda a desenvvolver melhor evitando ficar estagnado por muito tempo. 
+
+6 - TESTAR
+O codigo deve ser testdo a fim de que se identifique pontos falhos que possam ser melhorados. 
+
+7 - REFATORAR 
+A ação de refatorar consiste em observar o código pronto e pesquisar maneiras para melhora-lo de modo que o mesmo fique cada vez menor e exercendo sempre as mesmas funções. 
+...
+
+`
