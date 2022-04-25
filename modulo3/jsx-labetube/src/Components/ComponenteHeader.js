f7dd99f8366a655eaed15c007a90a8fef82bf249
RodrigoLisboa6 <lisboasilva.educ@gmail.com>
2022-04-25 18:13:22 -0300

    Enviando exercício lebetube.jsx

---
+++ modulo3/jsx-labetube/src/Components/ComponenteHeader.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// const Header = () => {
+//     return (
+//         <Header>
+//             <h1 LabeTube />
+//             <input type="text" placeholder="Busca" id="campoDeBusca" />
+//         </Header>
+//     )
+// }
+
+// export default Header
+
---
+++ modulo3/jsx-labetube/src/Components/MenuItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const MenuItem = (props) => {
+    return(
+        <li> {props.item} </li>
+    )
+}
+
+export default MenuItem
